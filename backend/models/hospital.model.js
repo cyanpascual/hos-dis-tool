3d40030ff5fbd1f87b3255f26d35475ac573e604
kingd0mz <dcfargas@up.edu.ph>
2020-03-26 21:20:15 +0800

    first trial

--- backend/models/hospital.model.js
+++ backend/models/hospital.model.js
@@ -3,11 +3,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const hospitalSchema = new Schema({
-  hospital: {type: String, required: true, unique: true, trim: true, minlength: 3},
-  username: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: Date, required: true },
+  type: "Feature",
+  typeofFeat: "hospital",
+  properties: {
+    Name_of_Ho: {type: String, required: true, unique: true, trim: true, minlength: 3},
+    Address: {type: String, required: true, trim: true},
+    Google_Plu: {type: String, required: true, trim: true},
+    Supply_Cap: {type: String, required: true, trim: true},
+    Supply_Cur: {type: String, required: true, trim: true},
+    Head: {type: String, required: true, trim: true},
+    Website: {type: String, required: true, trim: true},
+    Contact_Num: {type: String, required: true, trim: true},
+  },
+  geometry: {
+    type: "Point",
+    coordinates: {type: String, required: true, trim: true},
+  },
 }, {
   timestamps: true,
 });
--- backend/routes/hospitals.js
+++ backend/routes/hospitals.js
@@ -3,14 +3,39 @@ let Hospital = require('../models/hospital.model');
 
 router.route('/').get((req, res) => {
   Hospital.find()
-    .then(users => res.json(users))
+    .then(hospitals => res.json(hospitals))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
-  const hospitalname = req.body.hospitalname;
+  const Name_of_Ho = req.body.Name_of_Ho;
+  const Address = req.body.Address;
+  const Google_Plu = req.body.Google_Plu;
+  const Supply_Cap = req.body.Supply_Cap;
+  const Supply_Cur = req.body.Supply_Cur;
+  const Head = req.body.Head;
+  const Website = req.body.Website;
+  const Contact_Num = Contact_Num;
+  const coordinates = req.body.coordinates;
 
-  const newHospital = new Hospital({hospitalname});
+  const newHospital = new Hospital({
+    type: "Feature",
+    typeofFeat: "hospital",
+    properties: {
+      Name_of_Ho,
+      Address,
+      Google_Plu,
+      Supply_Cap,
+      Supply_Cur,
+      Head,
+      Website,
+      Contact_Num,
+    },
+    geometry: {
+      type: "Point",
+      coordinates,
+    },
+  });
 
   newHospital.save()
     .then(() => res.json('Hospital added!'))
