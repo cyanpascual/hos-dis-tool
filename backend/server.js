73308a4f1141ca5c982d3f78cfb07822e9859cbc
kingd0mz <dcfargas@up.edu.ph>
2020-03-27 18:13:22 +0800

    Added Backend #1: Fxns (Get Hospitals, Add/Update/Delete Hospital)

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://dbUser:dbUserPassword@trams-fjjjm.gcp.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const hospitalsRouter = require('./routes/hospitals');
+
+app.use('/hospitals', hospitalsRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
