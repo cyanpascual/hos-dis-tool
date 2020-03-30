4caba2fce33cf3c403cc769d549c5de3455069e0
kingd0mz <dcfargas@up.edu.ph>
2020-03-31 01:20:06 +0800

    'connected to mLab insteadd of MongoDB Atlas'

--- server.js
+++ server.js
@@ -14,28 +14,7 @@ if(process.env.NODE_ENV === 'production'){
 
 app.use(cors());
 app.use(express.json());
-<<<<<<< HEAD
-
-//MongoDB connection string
-const uri = "mongodb+srv://rvramos:rvramosPassword@trams-2qp2z.gcp.mongodb.net/test?retryWrites=true&w=majority"
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*',(req, res)=>{
-      res.sendFile(path.join(__dirname,'client','build','index.html'));
-  });
-
-}
-=======
 app.use(express.urlencoded({extended: true}));
->>>>>>> 9a6b4dd94174da8de2db9811235ebe160ccb9610
 
 const hospitalsRouter = require('./routes/hospitals');
 const facilityRouter = require('./routes/facility');
