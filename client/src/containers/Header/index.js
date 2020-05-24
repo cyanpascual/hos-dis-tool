157e03e8d7bc5bd13efbd504032171de8306b4af
Cyan Erick Pascual <cbpascual3@up.edu.ph>
2020-05-24 19:02:57 +0800

    revert back to 3fa39f588d9420f98b8f6a85cb8f1723f380f012

--- client/src/containers/Header/index.js
+++ client/src/containers/Header/index.js
@@ -2,9 +2,6 @@ import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Avatar from '@material-ui/core/Avatar';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import HelpIcon from '@material-ui/icons/Help';
 import Hidden from '@material-ui/core/Hidden';
@@ -23,7 +20,7 @@ import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import Toolbar from '@material-ui/core/Toolbar';
 import Tooltip from '@material-ui/core/Tooltip';
-import Typography from '@material-ui/core/Typography';
+
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { withStyles } from '@material-ui/core/styles';
 import tramslogo from '../../assets/logos/tramsLogo.png';
@@ -38,7 +35,6 @@ import VolunteerDialog from '../VolunteerDialog';
 import UpdateDialog from '../UpdateDialog'
 import { FeaturesContext } from '../../contexts/FeaturesContext';
 import { MapsContext } from '../../contexts/MapsContext';
-import CardActions from '@material-ui/core/CardActions';
 
 
 
@@ -68,13 +64,8 @@ const styles = (theme) => ({
 
 function Header(props) {
   const { classes, onDrawerToggle } = props;
-<<<<<<< HEAD
-  const { hospitalList,compareValues } = useContext(FeaturesContext);
-  const {setSelectedHospital, goToSelected,   selectedHospital} = useContext(MapsContext)
-=======
   const { hospitals, resetHospitals, hospitalList, setFilterSetting, filterSetting, filterLevel, setFilterLevel,compareValues } = useContext(FeaturesContext);
   const {setSelectedHospital, goToSelected} = useContext(MapsContext)
->>>>>>> parent of 3fa39f5... replaced markers and removed  some  unused variables
   var hospitalNames =[]
   var prioritizedHospitals=[]
   if(hospitalList){
