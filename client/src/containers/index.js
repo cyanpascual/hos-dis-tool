3db9852c9faa1bed46d70a59d51184a8ef32dbd4
pamucchi <pgtodoc@up.edu.ph>
2020-06-14 15:47:56 +0800

    login
    deleted unnecessary files (2)

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,22 +1,14 @@
 import React, {useContext, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from "react-redux";
 import axios from 'axios';
 import { createMuiTheme} from '@material-ui/core/styles';
 import 'leaflet/dist/leaflet.css'
 import 'typeface-roboto';
 import { FeaturesContext } from '../contexts/FeaturesContext';
 import Login from './login';
-import Dashboard from './login/dashboard';
 import Main from './main';
 import NotFound from './notFound';
-import PrivateRoute from "./login/PrivateRoute"
 
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./login/utils";
-
-import { setCurrentUser, logoutUser } from "./login/redux/actions/authActions";
-import store from "./login/redux/store";
 
 
 let theme = createMuiTheme({
@@ -159,25 +151,6 @@ const styles = {
 };
 
 
-if (localStorage.jwtToken) {
-  // Set auth token header auth
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
-  // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
-  // Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
-  if (decoded.exp < currentTime) {
-    // Logout user
-    store.dispatch(logoutUser());
-
-    // Redirect to login
-    window.location.href = "./login";
-  }
-}
-
 function App(props) {
   const { classes } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -237,18 +210,16 @@ function App(props) {
   }, [])
 
   return(
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route path='/' component={Main} exact/>
-          <Route path='/login' component={Login}/>      
-          <PrivateRoute path='/dashboard' component={Dashboard}/>
-          <Route path='*'>
-            <NotFound/>
-          </Route>
-        </Switch>
-      </Router>
-    </Provider>
+    <Router>
+      <Switch>
+        <Route path='/' component={Main} exact/>
+        <Route path='/login' component={Login}/>      
+        <Route path='*'>
+          <NotFound/>
+        </Route>
+      </Switch>
+    </Router>
+
   );
 }
 
